071561ba845ed2d040e824b247639a8ac68f241f
Glitched1221 <leostevens008@gmail.com>
2025-05-21 10:54:56 +0100

    car can moive cant turn

--- Assets/WheelControl.cs
+++ Assets/WheelControl.cs
@@ -9,30 +9,77 @@ public class WheelControl : MonoBehaviour
     [SerializeField] WheelCollider frontLeft;
     [SerializeField] WheelCollider backRight;
     [SerializeField] WheelCollider backLeft;
+
+    [SerializeField] Transform frontRightTransform;
+    [SerializeField] Transform frontLeftTransform;
+    [SerializeField] Transform backRightTransform;
+    [SerializeField] Transform backLeftTransform;
+
+    Transform carBody;
+
     public float acceleration = 500f;
     public float breakingForce = 300f;
+    public float maxTurnAngle = 15;
+
 
     private float currentAcceleration = 0f;
     private float currentBreakForce = 0f;
+    private float currentTurnAngle = 0f;
+
+    public float Speed;
 
     private void FixedUpdate()
     {
 
-        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+   
+        currentAcceleration = acceleration * Input.GetAxis("Vertical") ;
         if (Input.GetKey(KeyCode.Space))
+        {
+
+  
         
-            currentBreakForce = breakingForce;
-            else 
-            currentBreakForce = 0f;
-        
+            currentBreakForce = breakingForce;}
+            else {
+
+           
+            currentBreakForce = 0f; }
+              
         frontRight.motorTorque = currentAcceleration;
         frontLeft.motorTorque = currentAcceleration;
+        //backRight.motorTorque = currentAcceleration;
+        //backLeft.motorTorque = currentAcceleration;
 
         frontRight.brakeTorque = currentBreakForce;
         frontLeft.brakeTorque = currentBreakForce;
         backLeft.brakeTorque = currentBreakForce;
         backRight.brakeTorque = currentBreakForce;
+
+
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+         backLeft.steerAngle = currentTurnAngle;
+         backRight.steerAngle = currentTurnAngle;
+
+        UpdateWheel(frontLeft, frontLeftTransform);
+        UpdateWheel(frontRight, frontRightTransform);
+        UpdateWheel(backLeft, backLeftTransform);
+        UpdateWheel(backRight, backRightTransform);
+
+      
+
     }
 
+    void UpdateWheel(WheelCollider col, Transform trans)
+    {
+
+        Vector3 position;
+        Quaternion rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        trans.position = position;
+        trans.rotation = rotation;
+    }
 
 }
